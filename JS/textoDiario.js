d19a9c491703090b9ecdd7820d1930f5d7d4d642
Patrick Lorêdo <phlsjw@gmail.com>
2026-08-07 14:04:56 -0300

    iniciando dashboard texto Diario

--- JS/textoDiario.js
+++ JS/textoDiario.js
@@ -1,14 +1,32 @@
-
 window.onload = function () {
-    mostraDataAtual('inputDateTextoDiario')
+    alteraData('inputDateTextoDiario','0');
 };
 
-function mostraDataAtual(idCampo){
-    var campoData = document.getElementById(idCampo);
-    var data = new Date();
-    var dia = String(data.getDate()).padStart(2,'0');
-    var mes = String(data.getMonth()+1).padStart(2,'0');
-    var ano = data.getFullYear();
+function alteraData(idCampo, escolha) {
+    let campoData = document.getElementById(idCampo);
+    let data;
+
+    if (campoData.value) {
+        data = new Date(campoData.value + "T00:00:00");
+    } 
+    else {
+        data = new Date();
+    }
+
+    if (escolha === '-1') {
+        data.setDate(data.getDate() - 1);
+    }
+    else if (escolha === '0') {
+        data = new Date();
+    }
+    else if (escolha === '1') {
+        data.setDate(data.getDate() + 1);
+    }
+
+    let dia = String(data.getDate()).padStart(2, '0');
+    let mes = String(data.getMonth() + 1).padStart(2, '0');
+    let ano = data.getFullYear();
 
     campoData.value = `${ano}-${mes}-${dia}`;
 }
+
