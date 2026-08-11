139c4b9646a1826c71acdd7e08e1f4c1d2cb1386
Patrick Lorêdo <phlsjw@gmail.com>
2026-08-11 12:13:02 -0300

    -

--- JS/vidaMinisterio.js
+++ JS/vidaMinisterio.js
@@ -63,7 +63,7 @@ function exibeQtdTextArea(qtd, localExibe) {
 
         // ===== COLUNA 2 - Anotações do Discurso Inicial =====
         const colAnotacoesDiscursoInicial = document.createElement("div");
-        colAnotacoesDiscursoInicial.classList.add("col-12", "mb-5");
+        colAnotacoesDiscursoInicial.classList.add("col-12", "mb-3");
 
         const grupoAnotacoesDiscursoInicial = document.createElement("div");
         grupoAnotacoesDiscursoInicial.classList.add("input-group");
