a03ccae85ce511988cc0b6bf42e32775a18293c5
Patrick Lorêdo <phlsjw@gmail.com>
2026-08-13 16:31:42 -0300

    -

--- JS/saidasCampo.js
+++ JS/saidasCampo.js
@@ -19,3 +19,83 @@ window.onload = function () {
 
     popularLivrosBiblicos('livrosBiblicosSaidaCampo')
 };
+
+let livrosBiblicos = [
+    "Gênesis",
+    "Êxodo",
+    "Levítico",
+    "Números",
+    "Deuteronômio",
+    "Josué",
+    "Juízes",
+    "Rute",
+    "1 Samuel",
+    "2 Samuel",
+    "1 Reis",
+    "2 Reis",
+    "1 Crônicas",
+    "2 Crônicas",
+    "Esdras",
+    "Neemias",
+    "Ester",
+    "Jó",
+    "Salmos",
+    "Provérbios",
+    "Eclesiastes",
+    "Cânticos",
+    "Isaías",
+    "Jeremias",
+    "Lamentações",
+    "Ezequiel",
+    "Daniel",
+    "Oseias",
+    "Joel",
+    "Amós",
+    "Obadias",
+    "Jonas",
+    "Miquéias",
+    "Naum",
+    "Habacuque",
+    "Sofonias",
+    "Ageu",
+    "Zacarias",
+    "Malaquias",
+    "Mateus",
+    "Marcos",
+    "Lucas",
+    "João",
+    "Atos dos Apóstolos",
+    "Romanos",
+    "1 Coríntios",
+    "2 Coríntios",
+    "Gálatas",
+    "Efésios",
+    "Filipenses",
+    "Colossenses",
+    "1 Tessalonicenses",
+    "2 Tessalonicenses",
+    "1 Timóteo",
+    "2 Timóteo",
+    "Tito",
+    "Filemom",
+    "Hebreus",
+    "Tiago",
+    "1 Pedro",
+    "2 Pedro",
+    "1 João",
+    "2 João",
+    "3 João",
+    "Judas",
+    "Apocalipse"
+];
+
+function popularLivrosBiblicos(id) {
+    let campoPopular = document.getElementById(id);
+
+    for (let i = 0; i < livrosBiblicos.length; i++) {
+        let option = document.createElement('option');
+        option.value = livrosBiblicos[i];
+        option.innerText = livrosBiblicos[i];
+        campoPopular.appendChild(option);
+    }
+}
